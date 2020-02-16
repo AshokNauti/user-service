571193520206e2fdba817d847d8333271280208c
Ashok Nautiyal <ashok.nautiyal@platformblack.com>
2020-02-16 22:40:02 +0000

    updated

--- user-service/src/main/java/com/aitsl/user/userservice/UserController.java
+++ user-service/src/main/java/com/aitsl/user/userservice/UserController.java
@@ -3,8 +3,10 @@ package com.aitsl.user.userservice;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.config.environment.Environment;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,20 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-	//@Autowired
-	//private Environment environment;
+	@Autowired
+	private Environment environment;
 	
 	@Autowired
 	private UserDetailRepository userDetailRepository;
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@GetMapping("users/{id}")
-	public Optional<UserDetail> getUserInfo(@PathVariable long id) {
+	public Optional<UserDetail> getUserDetail(@PathVariable long id) {
 		
-		return userDetailRepository.findById(id);
+		Optional<UserDetail> userDetail = userDetailRepository.findById(id);
+		
+		if(userDetail.isPresent()) {
+			logger.info("User: " + userDetail);
+			userDetail.get().setPort(environment.getProperty("local.server.port"));
+		}
+		
+		return userDetail;
 	}
 	
 	@GetMapping("users")
 	public List<UserDetail> getUsers() {
+		
+		logger.info("All Users: ");
 		
 		return userDetailRepository.findAll();
 	}
--- user-service/src/main/java/com/aitsl/user/userservice/UserDetail.java
+++ user-service/src/main/java/com/aitsl/user/userservice/UserDetail.java
@@ -12,6 +12,23 @@ public class UserDetail {
 	private String lastName;
 	private String email;
 	private String type;
+	private String port;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
 
 	public long getId() {
 		return id;
